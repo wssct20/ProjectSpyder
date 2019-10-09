a03b4f638b1621ebf69ef324b5b0234623555607
programminghoch10 <hoch10@kabelbw.de>
2019-10-09 20:14:53 +0200

    updated system.php

--- system.php
+++ system.php
@@ -2,7 +2,7 @@
 require_once("functions.php");
 
 $db = new mysqli($sqlhost, $sqluser, $sqlpass, $sqldbname);
-if ($mysqli->connect_errno) {
+if ($db->connect_errno) {
     //die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
 	die("ERRSQLDB");
 }
