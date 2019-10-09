557fcf184e898f020ada3689bc0fc273a7513050
programminghoch10 <hoch10@kabelbw.de>
2019-10-09 19:23:37 +0200

    updated test files

---
+++ mysqlinfo.php
@@ -0,0 +1,5 @@
+<?php
+require_once("system.php");
+echo $db->host_info."\n";
+
+?>
--- system.php
+++ system.php
@@ -3,9 +3,8 @@ require_once("functions.php");
 
 $db = new mysqli($sqlhost, $sqluser, $sqlpass, $sqldbname);
 if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-	die();
+    //die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+	die("ERRSQLDB");
 }
-echo $mysqli->host_info . "\n";
 
 ?>
