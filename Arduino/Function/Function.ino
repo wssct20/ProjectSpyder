e5e6eb4d22526b175aa32c649a5ee1276ee04e64
jjw2202 <jjw2202@online.de>
2019-10-19 12:41:19 +0200

    Update Function.ino

--- Arduino/Function/Function.ino
+++ Arduino/Function/Function.ino
@@ -1,13 +1,23 @@
 
+const int buttonPin = 5;
 
 void setup() {
 
   wifi();
+  php();
+
+  pinMode(buttonPin, INPUT);
 
 }
 
 void loop() {
 
-  
-  
+  if (digitalRead(buttonPin) == HIGH)
+  {
+    Serial.println("button LOW");
+  }
+  else {
+    Serial.println("button HIGH");
+  }
+  delay(1000);
 }
