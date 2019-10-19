c172aa17546836c0884fe8fce5e260baca56dcfe
jjw2202 <jjw2202@online.de>
2019-10-19 19:14:17 +0200

    Create LEDs.ino

---
+++ Arduino/LEDs/ESP32_code_filepath.h
@@ -0,0 +1,9 @@
+/*
+ * Insert here the file path of the mentioned files
+ */
+
+//...\ProjectSpyder\Arduino\ESP32_consistent_code_parts\Wifi.ino
+#include "C:\Users\Jannik\Documents\GitHub\ProjectSpyder\Arduino\ESP32_consistent_code_parts\wifi.ino"
+
+//...\ProjectSpyder\Arduino\ESP32_consistent_code_parts\PHP.ino
+#include "C:\Users\Jannik\Documents\GitHub\ProjectSpyder\Arduino\ESP32_consistent_code_parts\php.ino"
---
+++ Arduino/LEDs/LEDs.ino
@@ -0,0 +1,15 @@
+#include "ESP32_code_filepath.h"
+
+
+void setup() {
+  
+  Serial.begin(115200);
+
+  wifi();
+  php();
+
+}
+
+void loop() {
+  
+}
