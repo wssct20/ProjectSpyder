58bb2f6fe50bdeef4da51637b432571e8f80322c
jjw2202 <52346420+jjw2202@users.noreply.github.com>
2019-10-19 20:06:52 +0200

    Delete ESP32_code_filepath.h

--- Arduino/LEDs/ESP32_code_filepath.h
+++
@@ -1,9 +0,0 @@
-/*
- * Insert here the file path of the mentioned files
- */
-
-//...\ProjectSpyder\Arduino\ESP32_consistent_code_parts\Wifi.ino
-#include "C:\Users\Jannik\Documents\GitHub\ProjectSpyder\Arduino\ESP32_consistent_code_parts\wifi.ino"
-
-//...\ProjectSpyder\Arduino\ESP32_consistent_code_parts\PHP.ino
-#include "C:\Users\Jannik\Documents\GitHub\ProjectSpyder\Arduino\ESP32_consistent_code_parts\php.ino"
