887038bef524dde218e2dc81d2a6037980c14ec0
programminghoch10 <hoch10@kabelbw.de>
2019-10-24 18:42:26 +0200

    fixed sql error not displaying properly

--- system.php
+++ system.php
@@ -39,7 +39,7 @@ require("credentials.php");
 
 $db = new mysqli($sqlhost, $sqluser, $sqlpass, $sqldbname);
 if ($db->connect_errno) {
-	die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+	die("Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error);
 	die("ERRSQLDB");
 }
 
