c46450ff4e9111b7a513f4bd4ff385b519f66d7c
programminghoch10 <hoch10@kabelbw.de>
2019-10-24 16:34:45 +0200

    Update arduinotest.php

--- arduinotest.php
+++ arduinotest.php
@@ -1,6 +1,8 @@
 <?php
-$get = $_REQUEST["get"] ?? "";
-$post = $_POST["post"] ?? "";
-print_r($get);
-print_r($post);
+//$get = $_REQUEST["get"] ?? "";
+//$post = $_POST["post"] ?? "";
+//print_r($get);
+//print_r($post);
+print_r($_REQUEST);
+print_r($_POST);
 ?>
