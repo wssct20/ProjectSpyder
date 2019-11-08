c3e0f77661e6a7dc4f0bcd2bc960662ebb2387d9
jjw2202 <jjw2202@online.de>
2019-11-08 18:49:35 +0100

    added addressable rgb led strip

--- Arduino/rgbledstrip.ino
+++ Arduino/rgbledstrip.ino
@@ -1,8 +1,26 @@
+/*#include <Adafruit_NeoPixel.h>
+
+#define datapin 5         //Pin to which the data pin of the strip is connected
+#define lednumber 60      //Number of connected LEDs
+
+Adafruit_NeoPixel ledstrip(lednumber, datapin, NEO_GRB + NEO_KHZ800);
 
 void rgbledstripsetup() {
+
+  ledstrip.begin();
+  ledstrip.clear();
   
 }
 
 void rgbledstriploop() {
+
+  for (int i = 0; i < lednumber; i++)
+  {
+    ledstrip.setPixelColor(i, ledstrip.Color(0, 150, 0));
+
+    ledstrip.show();
+
+    delay(500);
+  }
   
-}
+}*/
