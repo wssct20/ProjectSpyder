003090857c90ccab2b377ef207ad6c4952589deb
programminghoch10 <hoch10@kabelbw.de>
2019-11-09 19:46:21 +0100

    added state check syntax

--- specific.php
+++ specific.php
@@ -3,15 +3,23 @@
 function specificactions($device, $requesttype, $returnstack) {
 	//specificactions: actions specific to certain types
 	switch ($device["type"]) {
-		case "button":
-			
-		case "addressablergbledstrip":
-			
+		
 		default:
 			//no action needed
 			break;
 	}
 }
 
+function checkstatesyntax($state, $type) {
+	switch ($type) {
+		case "button":
+			if (!sizeof(explode("", $state)) == 1) return false;
+			if (!($state == 1 || $state == 0)) return false;
+			return true;
+		default:
+			//no check needed
+			return true;	
+	}
+}
 
 ?>
