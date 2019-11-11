fa34165b7b5cd92c4b1ac6743192502690bc4e76
programminghoch10 <hoch10@kabelbw.de>
2019-11-11 22:33:54 +0100

    added gui devices table

--- GUI/devices.php
+++ GUI/devices.php
@@ -17,8 +17,38 @@ checksession();
 		</style>
 	</head>
 	<body>
-	<h1>ProjectSpyder Devices</h1>
-	
-	<table></table>
+		<h1>ProjectSpyder Devices</h1>
+		
+		<table>
+			<?php 
+			//devices array: columns of the devices table to show. format: Visible text => column name of table devices.
+			$columns = array(
+				"ID" => "id",
+				"Name" => "name",
+				"Type" => "type",
+				"IP-Address" => "ipaddress",
+			);
+			$devices = getdevices();
+			if (sizeof($devices) == 0) {
+				die("Error: No devices found.");
+			}
+			echo "<tr>";
+			foreach ($columns as $key => $column) {
+				echo "<th>";
+				echo $key;
+				echo "</th>";
+			}
+			echo "</tr>";
+			foreach ($devices as $device) {
+				echo "<tr>";
+				foreach ($columns as $column) {
+					echo "<td>";
+					echo $device[$column];
+					echo "</td>";
+				}
+				echo "</tr>";
+			}
+			?>
+		</table>
 	</body>
 </html>
