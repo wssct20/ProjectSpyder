9d8e8da240e02fe0ff191d7de78ecd3f784e7c9b
jjw2202 <jjw2202@online.de>
2019-11-14 21:54:17 +0100

    Create rotation.ino

---
+++ Arduino/rotation.ino
@@ -0,0 +1,31 @@
+/*#include "Adafruit_BNO055.h"
+
+/*
+ * Connection:
+ * VDD  to 3.3V-5V DC
+ * GND  to GND (common ground)
+ * SCL  to the I²C SCL (at the ESP32 GPIO 22)
+ * SDA  to the I²C SDA (at the ESP32 GPIO 21)
+ */
+/*
+//initialise the sensor
+Adafruit_BNO055 rotation = Adafruit_BNO055(55);
+
+void rotationsetup() {
+
+  //start the sensor communication
+  if (rotation.begin())
+  {
+    Serial.println("Found rotation sensor");
+  }
+  else
+  {
+    Serial.println("No rotation sensor found");
+    hibernate(60*60*24);
+  }
+  
+}
+
+void rotationloop() {
+  
+}*/
