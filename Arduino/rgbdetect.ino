3ada07953d537a12b67b6d7ad00f91c8397d6b6c
jjw2202 <jjw2202@online.de>
2019-11-14 18:50:14 +0100

    added sensor led control in rgbdetect

--- Arduino/rgbdetect.ino
+++ Arduino/rgbdetect.ino
@@ -1,4 +1,3 @@
-//#include "Wire.h"
 #include "Adafruit_TCS34725.h"
 
 /*
@@ -9,11 +8,15 @@
  * SDA  to the I²C SDA (at the ESP32 GPIO 21)
  */
 
-//Initialise the sensor
+//initialise the sensor
 Adafruit_TCS34725 rgbdetect = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
+
+//define the sensor led pin
+int detectledpin = 23;
 
 void rgbdetectsetup() {
 
+  //start the sensor communication
   if (rgbdetect.begin())
   {
     Serial.println("Found rgb sensor");
@@ -23,6 +26,10 @@ void rgbdetectsetup() {
     Serial.println("No rgb sensor found");
     hibernate(60*60*24);
   }
+
+  //initialise the sensor led
+  pinMode(detectledpin, OUTPUT);
+  digitalWrite(detectledpin, LOW);
   
 }
 
@@ -30,14 +37,17 @@ void rgbdetectloop() {
 
   uint16_t r, g, b, c, colorTemp, lux;
 
-  //TODO: LED einschalten
-  //TODO: delay250ms
+  //turn on the sensor led
+  digitalWrite(detectledpin, HIGH);
+  delay(250);
 
+  //measure the rgb values and calculate the themperature and brightness
   rgbdetect.getRawData(&r, &g, &b, &c);
   colorTemp = rgbdetect.calculateColorTemperature_dn40(r, g, b, c);
   lux = rgbdetect.calculateLux(r, g, b);
 
-  //TODO: LED ausschalten
+  //turn off the sensor led
+  digitalWrite(detectledpin, LOW);
 
   Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
   Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
@@ -61,6 +71,8 @@ void rgbdetectloop() {
 
   Serial.print("state: ");
   Serial.println(state);
+  
+  //send the values to the server
   putstate(state);
 
   lightsleep(requesttimeout);
