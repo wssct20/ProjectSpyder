d9cb9b65ceb2b660034703915dcf0b18af2a3c8a
programminghoch10 <hoch10@kabelbw.de>
2020-02-06 15:42:21 +0100

    added tiles

---
+++ GUI/tilegrid.php
@@ -0,0 +1,23 @@
+<?php 
+require_once("system.php");
+if (!sessionvalid()) die("INVALIDSESSION");
+
+
+
+$devices = getdevices();
+?>
+
+<div class="tilegrid">
+	
+	<?php 
+	foreach ($devices as $device) {
+		?>
+		<div class="tile">
+			<h1><?php echo (empty($device["name"]) ? $device["type"]." ".$device["id"] : $device["name"]);?></h1>
+			<?php echo gettile($device["type"], getdata($device["id"])); ?>
+		</div>
+		<?php
+	}
+	?>
+	
+</div>
