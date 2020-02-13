582887411b5d8dc68cf6e8dc16493419cfdc744c
programminghoch10 <hoch10@kabelbw.de>
2020-02-13 08:59:21 +0100

    disabled type check in pair

--- pair.php
+++ pair.php
@@ -11,10 +11,10 @@ $type = $_REQUEST["type"];
 $returnstack = array();
 
 //check if type is valid and supported by the system
-$typevalid = checktype($type);
-if (!$typevalid) {
+// temporarily disabled, as any type will be supported with JSON data
+/*if (!checktype($type)) {
 	dieerror("TYPEINVALID", "Client type not recognized by the system.");
-}
+}*/
 
 $ip = getipaddress();
 $pairtime = $now;
