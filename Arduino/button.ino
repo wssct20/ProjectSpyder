e175a82241c10b3c4e06458b0f99f511ea261609
jjw2202 <52346420+jjw2202@users.noreply.github.com>
2020-03-03 11:07:22 +0100

    added friendly datavalue of button

--- Arduino/button.ino
+++ Arduino/button.ino
@@ -2,11 +2,16 @@
 #define buttonPin 4
 //#define touchthreshold 40
 
+const String friendlybuttonstate[] = {
+  "Released",
+  "Pressed",
+};
+
 void buttonsetup() {
   
   pinMode(buttonPin, INPUT_PULLUP);
 
-  jsonstructure = "";
+  jsonstructure = "{\"data\":{\"data\":{\"state\":0},\"friendly\":{\"datavar\":{\"state\":\"button state\"},\"datavalue\":{\"state\":\"Released\"},\"button\":\"Button\"},\"preferredupdatetime\":120}}";
 
 }
 
@@ -16,11 +21,13 @@ void buttonloop() {
   
   DynamicJsonDocument datadoc(JSONCAPACITY);
   JsonObject data = datadoc.createNestedObject("data");
+  JsonObject friendly = datadoc.createNestedObject("friedly");
   uint8_t buttonstate = !digitalRead(buttonPin);
   #ifdef debugmode
     Serial.println("buttonstate: " + String(buttonstate));
   #endif
   data["state"] = buttonstate;
+  friendly["datavalue"] = friendlybuttonstate[buttonstate];
   serializeJson(datadoc, buttondata);
   #ifdef debugmode
     Serial.println("JSON: " + String(buttondata));
