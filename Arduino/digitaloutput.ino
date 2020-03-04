2af2c43ed03fdd61d49220f07f3396a90608d15b
jjw2202 <52346420+jjw2202@users.noreply.github.com>
2020-03-04 14:08:59 +0100

    added friendly in digitaloutput

--- Arduino/digitaloutput.ino
+++ Arduino/digitaloutput.ino
@@ -11,15 +11,17 @@ void digitaloutputsetup() {
 
 void digitaloutputloop() {
 
+  String digitaloutputdata;
+
   #ifdef debugmode
     Serial.println("digitaloutput() look for data");
   #endif
 
   //get data from system
-  DynamicJsonDocument data(JSONCAPACITY);
-  deserializeJson(data, getdata());
+  DynamicJsonDocument receiveddata(JSONCAPACITY);
+  deserializeJson(receiveddata, getdata());
 
-  int outputstate = data["data"]["state"].as<int>();
+  int outputstate = receiveddata["data"]["state"].as<int>();
 
   //check data
   if (outputstate != 1) outputstate = 0;
@@ -30,6 +32,24 @@ void digitaloutputloop() {
 
   //set output
   digitalWrite(output_pin, outputstate);
+
+  DynamicJsonDocument datadoc(JSONCAPACITY);
+  JsonObject friendly = datadoc.createNestedObject("friendly");
+  JsonObject datavalue = friendly.createNestedObject("datavalue");
+  const String friendlystate[] = {
+    "off",
+    "on",
+  };
+  datavalue["state"] = friendlystate[outputstate];
+
+  serializeJson(datadoc, digitaloutputdata);
+
+  #ifdef debugmode
+    Serial.println("JSON: " + String(digitaloutputdata));
+  #endif
+  
+  //send the values to the server
+  updatedata(digitaloutputdata);
 
   //go to lightsleep
   lightsleep(requesttimeout);
