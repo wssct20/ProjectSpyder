c72e7c59efd662ee620a9268dfae3c83f233600d
programminghoch10 <hoch10@kabelbw.de>
2020-03-04 14:52:17 +0100

    adopted tiles in main.php to JSON

--- GUI/tilegrid.php
+++ GUI/tilegrid.php
@@ -2,9 +2,22 @@
 require_once("system.php");
 if (!sessionvalid()) die("INVALIDSESSION");
 
-include_once("specific.php");
-
 $devices = getdevices();
+
+function gettile($device) {
+	//gettile: needs to output the data userfriendly for the GUI
+	$data = getdata($device);
+	$returnstring = "<table>";
+	foreach ($data["data"] as $datafield) {
+		$returnstring = $returnstring."<tr>";
+		$returnstring = $returnstring."<td>".sanitizehtml($data["friendy"]["datavar"][$datafield] ?? $datafield)."</td>";
+		$returnstring = $returnstring."<td>".sanitizehtml($data["friendy"]["datavalue"][$datafield] ?? $data["data"][$datafield])."</td>";
+		$returnstring = $returnstring."</tr>";
+	}
+	$returnstring = $returnstring."</table>";
+	return $returnstring;
+}
+
 ?>
 
 <div class="tilegrid">
@@ -14,9 +27,7 @@ $devices = getdevices();
 		?>
 		<div class="tile">
 			<h1><?php echo (empty($device["name"]) ? ($friendlytypenames[$device["type"]] ?? $device["type"])." ".$device["id"] : $device["name"]);?></h1>
-			<!--
-			<?php echo gettile($device["type"], getdata($device["id"])); ?>
-			-->
+			<?php echo gettile($device); ?>
 		</div>
 		<?php
 	}
