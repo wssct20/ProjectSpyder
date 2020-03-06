e13f51e733ef6e0bb46fc8ca8960fb4c31d5427c
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2020-03-06 11:28:26 +0100

    added reset.php
    for debugging; can be called to reset entire database

---
+++ reset.php
@@ -0,0 +1,21 @@
+<?php
+    require_once("credentials.php");
+    $db = new mysqli($sqlhost, $sqluser, $sqlpass, $sqldbname);
+    if ($db->connect_errno) {
+        die("Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error);
+    }
+    //unprepared query: drop all tables
+    if (!$db->query(
+            "DECLARE @sql NVARCHAR(max)=''
+
+            SELECT @sql += ' Drop table ' + QUOTENAME(TABLE_SCHEMA) + '.'+ QUOTENAME(TABLE_NAME) + '; '
+            FROM   INFORMATION_SCHEMA.TABLES
+            WHERE  TABLE_TYPE = 'BASE TABLE'
+            
+            Exec Sp_executesql @sql"
+        )
+	) {
+		die("reset tables failed: (" . $db->errno . ") " . $db->error);
+    }
+    echo "RESET SUCCESSFUL";
+?>
