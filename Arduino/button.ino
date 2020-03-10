52c13f68a6278dea96b763be9f5f4a26203bcfc3
jjw2202 <52346420+jjw2202@users.noreply.github.com>
2020-03-10 14:08:07 +0100

    added gpio wakeup for button

--- Arduino/button.ino
+++ Arduino/button.ino
@@ -6,13 +6,25 @@ void buttonsetup() {
   
   pinMode(buttonPin, INPUT_PULLUP);
 
+  enum wake_up_cause {
+    UNDEFINED,
+    ALL,
+    EXT0,
+    EXT1,
+    TIMER,
+    TOUCH,
+    ULP,
+    GPIO,
+    UART,
+  };
+
   jsonstructure = "{\
 \"data\":{\
 \"state\":0\
 },\
 \"friendly\":{\
 \"datavar\":{\
-\"state\":\"button state\"\
+\"state\":\"button%20state\"\
 },\
 \"datavalue\":{\
 \"state\":\"Released\"\
@@ -32,8 +44,15 @@ void buttonloop() {
   JsonObject data = datadoc.createNestedObject("data");
   JsonObject friendly = datadoc.createNestedObject("friendly");
   JsonObject datavalue = friendly.createNestedObject("datavalue");
-  uint8_t buttonstate = !digitalRead(buttonPin);
+  uint8_t buttonstate;
+/*  if (esp_sleep_get_wakeup_cause() == GPIO) {
+    buttonstate  = 1;
+  }
+  else {
+    buttonstate = !digitalRead(buttonPin);
+  }*/
   //bool buttonstate = !lightsleepgpio(requesttimeout, buttonPin, 0);
+  buttonstate = !digitalRead(buttonPin);
   #ifdef debugmode
     Serial.println("buttonstate: " + String(buttonstate));
   #endif
@@ -53,5 +72,6 @@ void buttonloop() {
   updatedata(buttondata);
 
   lightsleep(requesttimeout);
+  //lightsleepgpio(requesttimeout, buttonPin, 0);
   
 }
