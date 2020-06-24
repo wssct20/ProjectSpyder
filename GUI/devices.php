de1d9a4f65284c082fc90fceb7cab0292c8b495e
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2020-06-24 15:30:39 +0200

    encapsulated device configs into tables

--- GUI/devices.php
+++ GUI/devices.php
@@ -85,46 +85,60 @@ if ($action == "details") {
 			<div class="settings">
 				<h2><?php echo $friendlytype." ".$device["id"]; echo empty($device["name"]) ? "" : " - ".$device["name"]; ?></h2>
 				<?php 
-					if (sizeof($data["usermodifiabledata"]) > 0) echo "<h3>Data</h3>";
-					foreach ($data["usermodifiabledata"] as $usermodifiabledatafield) {
-						?>
-							<form method=post>
-								<input type=text name=id value="<?php echo $device["id"]; ?>" style="display: none;">
-								<input type=text name=action value=overwritefield style="display: none;">
-								<input type=text name=category value=data style="display: none;">
-								<input type=text name=var value="<?php echo $usermodifiabledatafield; ?>" style="display: none;">
-								<label><?php echo sanitizehtml($data["friendly"]["datavar"][$usermodifiabledatafield] ?? $usermodifiabledatafield) ?>: </label>
-								<?php //TODO: add for= to label for accessibility ?>
-								<input type=text name=value value="<?php echo $data["data"][$usermodifiabledatafield]; ?>">
-								<input type=submit name=submit value="Overwrite">
-							</form>
-						<?php
+					if (sizeof($data["usermodifiabledata"]) > 0) {
+						echo "<h3>Data</h3>";
+						echo "<table>";
+						foreach ($data["usermodifiabledata"] as $usermodifiabledatafield) {
+							?>
+								<tr>
+									<form method=post>
+										<td><label><?php echo sanitizehtml($data["friendly"]["datavar"][$usermodifiabledatafield] ?? $usermodifiabledatafield) ?> </label></td>
+										<?php /* TODO: add for= to label for accessibility */ ?>
+										<td>
+											<input type=text name=id value="<?php echo $device["id"]; ?>" style="display: none;">
+											<input type=text name=action value=overwritefield style="display: none;">
+											<input type=text name=category value=data style="display: none;">
+											<input type=text name=var value="<?php echo $usermodifiabledatafield; ?>" style="display: none;">
+											<input type=text name=value value="<?php echo $data["data"][$usermodifiabledatafield]; ?>">
+										</td>
+										<td><input type=submit name=submit value="Overwrite"></td>
+									</form>
+								</tr>
+							<?php
+						}
+						echo "</table>";
 					}
 				?>
 				<?php 
 					if (sizeof($data["settings"]) > 0) {
 						echo "<h3>Settings</h3>";
+						echo "<table>";
 						foreach ($data["settings"] as $settingsfield => $settingsvalue) {
 							?>
-								<form method=post>
-									<input type=text name=id value="<?php echo $device["id"]; ?>" style="display: none;">
-									<input type=text name=action value=overwritesettingsfield style="display: none;">
-									<input type=text name=category value=settings style="display: none;">
-									<input type=text name=var value="<?php echo $settingsfield; ?>" style="display: none;">
-									<label><?php echo sanitizehtml($data["friendly"]["settingsvar"][$settingsfield] ?? $settingsfield) ?>: </label>
-									<?php //TODO: add for= to label for accessibility ?>
-									<input type=text name=value value="<?php echo $data["settings"][$settingsfield]; ?>">
-									<input type=submit name=submit value="Overwrite">
-								</form>
+								<tr>
+									<form method=post>
+										<td><label><?php echo sanitizehtml($data["friendly"]["settingsvar"][$settingsfield] ?? $settingsfield) ?> </label></td>
+										<?php //TODO: add for= to label for accessibility ?>
+										<td>
+											<input type=text name=id value="<?php echo $device["id"]; ?>" style="display: none;">
+											<input type=text name=action value=overwritesettingsfield style="display: none;">
+											<input type=text name=category value=settings style="display: none;">
+											<input type=text name=var value="<?php echo $settingsfield; ?>" style="display: none;">
+											<input type=text name=value value="<?php echo $data["settings"][$settingsfield]; ?>">
+										</td>
+										<td><input type=submit name=submit value="Overwrite"></td>
+									</form>
+								</tr>
 							<?php
 						}
+						echo "</table>";
 					}
 				?>
 				<h3>Manage</h3>
 				<form method=post>
 					<input type=text name=id value="<?php echo $device["id"]; ?>" style="display: none;">
 					<input type=text name=action value=rename style="display: none;">
-					<label for="devicename">Name: </label>
+					<label for="devicename">Name </label>
 					<input type=text name=name id="devicename"
 						placeholder="<?php echo $friendlytype." ".$device["id"]; ?>" 
 						value="<?php echo empty($device["name"]) ? "" : $device["name"]; ?>">
@@ -137,7 +151,7 @@ if ($action == "details") {
 					<input type=submit name=submit value="Delete">
 				</form>
 				<h4 style="margin-bottom: 0;">Debug</h4>
-				<p style="margin-top: 0;"><?php echo jsonencode($data); ?></p>
+				<p style="margin-top: 0; word-break: break-all;"><?php echo jsonencode($data); ?></p>
 			</div>
 		</body>
 	</html>
