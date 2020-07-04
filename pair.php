d8e6d54854e71a34d6ffb179710a6a2169c4c768
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2020-07-04 16:30:57 +0200

    fixed wrong saving of data in pair.php

--- pair.php
+++ pair.php
@@ -15,7 +15,10 @@ $authcode = calculateauthcode($type, $ip, $pairtime);
 
 adddevice($type, $ip, $pairtime, $now, $authcode);
 updatedevice(getdevicebyauthcode($authcode)["id"], $now, $ip);
-if (!empty($data)) updatedata(getdevicebyauthcode($authcode)["id"], $data);
+if (!empty($data)) {
+	if (jsondecode($data) == null) dieerror("DATAINVALID", "Data is invalid, maybe the JSON is not complete");
+    updatedata(getdevicebyauthcode($authcode)["id"], jsondecode($data));
+}
 
 
 $returnstack["requesttimeout"] = 5;
